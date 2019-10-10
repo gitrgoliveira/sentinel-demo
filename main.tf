758b862faeadff826e4306b628298cdde9ad50da
Ricardo Oliveira <ricardo@hashicorp.com>
2019-10-10 10:05:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,11 +48,41 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   subnet_id     = "${data.terraform_remote_state.network.outputs.subnets[0]}"
   tags = {
     Name = "test_server"
     owner = "StepStone"
     tag = "${data.vault_generic_secret.secret.data["message"]}"
   }
+}
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 2.0"
+
+  identifier = "demodb"
+
+  engine            = "mysql"
+  engine_version    = "5.7.19"
+  instance_class    = "db.t2.large"
+  allocated_storage = 5
+
+  name     = "demodb"
+  username = "user"
+  password = "YourPwdShouldBeLongAndSecure!"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  subnet_ids = data.terraform_remote_state.network.outputs.subnets
 }
